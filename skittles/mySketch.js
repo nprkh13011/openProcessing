fee01d7e38a4a91fb4d6d7c77e32728e06757cb6
Nidhi <55457971+nprkh13011@users.noreply.github.com>
2022-11-06 22:56:55 -0500

    Add files via upload

---
+++ skittles/mySketch.js
@@ -0,0 +1,263 @@
+function setup() {
+	simple();
+}
+
+function draw() {
+	//0
+	fill('yellow');
+	circle(0,0, 100); //x - 800 pixels, y - max is 600, size
+		fill('red');
+		circle(50,50,25);
+		fill('violet');
+		circle(150,50,25);
+		fill('indigo');
+		circle(250,50,25);
+	fill('green');
+	circle(100,0, 100);
+		fill('orange');
+		circle(150,150,25);
+	fill('blue');
+	circle(200,0, 100);
+		fill('yellow');
+		circle(250,250,25);
+	fill('indigo');
+	circle(300,0, 100);
+		fill('green');
+		circle(350,350,25);
+	fill('violet');
+	circle(400,0, 100);
+		fill('blue');
+		circle(450,450,25);
+	fill('red');
+	circle(500,0, 100);
+		fill('indigo');
+		circle(550,550,25);
+	fill('orange');
+	circle(600,0, 100);
+		fill('violet');
+		circle(650,550,25);
+	fill('yellow');
+	circle(700,0, 100);
+	fill('green');
+	circle(800,0, 100);
+		
+	
+	//100
+	fill('orange');
+	circle(0,100, 100); //x - 800 pixels, y - max is 600, size
+		fill('red');
+		circle(50,150,25);
+	fill('yellow');
+	circle(100,100, 100);
+		fill('orange');
+		circle(150,250,25);
+	fill('green');
+	circle(200,100, 100);
+		fill('yellow');
+		circle(250,350,25);
+	fill('blue');
+	circle(300,100, 100);
+		fill('green');
+		circle(350,450,25);
+	fill('indigo');
+	circle(400,100, 100);
+		fill('blue');
+		circle(450,550,25);
+		fill('blue');
+		circle(350,50,25);
+	fill('violet');
+	circle(500,100, 100);
+		fill('green');
+		circle(450,50,25);
+	fill('red');
+	circle(600,100, 100);
+		fill('yellow');
+		circle(550,50,25);
+	fill('orange');
+	circle(700,100, 100);
+		fill('orange');
+		circle(650,50,25);
+	fill('yellow');
+	circle(800,100, 100);
+		fill('red');
+		circle(750,50,25);
+	
+	//200
+	fill('red');
+	circle(0,200, 100);
+		fill('red');
+		circle(50,250,25);
+	fill('orange');
+	circle(100,200, 100); //x - 800 pixels, y - max is 600, size
+		fill('orange');
+		circle(150,350,25);
+	fill('yellow');
+	circle(200,200, 100);
+		fill('yellow');
+		circle(250,450,25);
+	fill('green');
+	circle(300,200, 100);
+		fill('green');
+		circle(350,550,25);
+	fill('blue');
+	circle(400,200, 100);
+		fill('blue');
+		circle(350,150,25);
+	fill('indigo');
+	circle(500,200, 100);
+		fill('green');
+		circle(450,150,25);
+	fill('violet');
+	circle(600,200, 100);
+		fill('yellow');
+		circle(550,150,25);
+	fill('red');
+	circle(700,200, 100);
+		fill('orange');
+		circle(650,150,25);
+	fill('orange');
+	circle(800,200, 100);
+		fill('red');
+		circle(750,150,25);
+	
+	//300
+	fill('violet');
+	circle(0,300, 100);
+		fill('red');
+		circle(50,350,25);
+	fill('red');
+	circle(100,300, 100);
+		fill('orange');
+		circle(150,450,25);
+	fill('orange');
+	circle(200,300, 100); //x - 800 pixels, y - max is 600, size
+		fill('yellow');
+		circle(250,550,25);
+	fill('yellow');
+	circle(300,300, 100);
+	fill('green');
+	circle(400,300, 100);
+	fill('blue');
+	circle(500,300, 100);
+		fill('green');
+		circle(450,250,25);
+	fill('indigo');
+	circle(600,300, 100);
+		fill('yellow');
+		circle(550,250,25);
+	fill('violet');
+	circle(700,300, 100);
+		fill('orange');
+		circle(650,250,25);
+	fill('red');
+	circle(800,300, 100);
+		fill('red');
+		circle(750,250,25);
+
+	//400
+	fill('indigo');
+	circle(0,400, 100);
+		fill('red');
+		circle(50,450,25);
+	fill('violet');
+	circle(100,400, 100);
+		fill('orange');
+		circle(150,550,25);
+	fill('red');
+	circle(200,400, 100); //x - 800 pixels, y - max is 600, size
+		fill('yellow');
+		circle(250,550,25);
+	fill('orange');
+	circle(300,400, 100); //x - 800 pixels, y - max is 600, size
+	fill('yellow');
+	circle(400,400, 100);
+	fill('green');
+	circle(500,400, 100);
+	fill('blue');
+	circle(600,400, 100);
+		fill('yellow');
+		circle(550,350,25);
+	fill('indigo');
+	circle(700,400, 100);
+		fill('orange');
+		circle(650,350,25);
+	fill('violet');
+	circle(800,400, 100);
+		fill('red');
+		circle(750,350,25);
+	
+	//500
+	fill('blue');
+	circle(0,500,100);
+		fill('red');
+		circle(50,550,25);
+	fill('indigo');
+	circle(100,500, 100);
+	fill('violet');
+	circle(200,500, 100);
+	fill('red');
+	circle(300,500, 100); //x - 800 pixels, y - max is 600, size
+	fill('orange');
+	circle(400,500, 100); //x - 800 pixels, y - max is 600, size
+	fill('yellow');
+	circle(500,500, 100);
+	fill('green');
+	circle(600,500, 100);
+	fill('blue');
+	circle(700,500, 100);
+		fill('orange');
+		circle(650,450,25);
+	fill('indigo');
+	circle(800,500, 100);
+		fill('red');
+		circle(750,450,25);
+	
+	
+	//600
+	fill('green');
+	circle(0,600,100);
+		fill('red');
+		circle(50,650,25);
+	fill('blue');
+	circle(100, 600,100);
+	fill('indigo');
+	circle(200,600, 100);
+	fill('violet');
+	circle(300,600, 100);
+	fill('red');
+	circle(400,600, 100); //x - 800 pixels, y - max is 600, size
+	fill('orange');
+	circle(500,600, 100); //x - 800 pixels, y - max is 600, size
+	fill('yellow');
+	circle(600,600, 100);
+	fill('green');
+	circle(700,600, 100);
+	fill('blue');
+	circle(800,600, 100);
+		fill('red');
+		circle(750,550,25);
+	
+/*
+-----------------------------NOTES---------------------------------------
+*/ 
+	
+	// square(400,300,100);	//(x,y - top left corner),size-all sides the same
+	// circle(400,300,100);
+	
+// 	//diagonal
+// 	circle(100,100,100);
+// 	circle(200,200,100);
+// 	circle(300,300,100);
+// 	circle(400,400,100);
+	
+// 	//straight down
+// 	circle(100,100,100);
+// 	circle(100,200,100);
+// 	circle(100,300,100);
+// 	circle(100,400,100);
+// 	circle(400,100,100);
+// 	circle(400,200,100);
+// 	circle(400,300,100);
+// 	circle(400,400,100);
+
+}
---
+++ warm&cool/mySketch.js
@@ -0,0 +1,51 @@
+function setup() {
+	simple();
+}
+
+function draw() {
+	fill('orchid');
+	circle(400, 300, 500);  //circle
+	fill('red');
+	triangle(400, 300, 200, 10, 100, 170); //tri (x1,y1,x2,y2,x3,y3)
+	
+	fill('green');
+	circle(400, 300, 450); //circ
+	fill('orange');
+	triangle(400, 300, 100, 350, 100, 160); //tri
+	
+	fill('lightblue');
+	circle(400, 300, 400); // cir
+	fill ('yellow') 
+	triangle(400, 300, 190, 500, 90, 350); //tri
+	
+	fill('lavender');
+	circle(400, 300, 350); // ci
+	fill ('lightpink') 
+	triangle(400, 300, 400, 570, 180, 500); //tri
+	
+  fill('lime');
+	circle(400, 300, 300); // c
+	fill ('coral') 
+	triangle(400, 300, 577, 490, 400, 580); //tri
+	
+	fill('indigo');
+	circle(400, 300, 250); // ci
+	fill ('magenta') 
+	triangle(400, 300, 660, 350, 575, 500); //tri
+	
+	fill('turquoise');
+	circle(400, 300, 200); // cir
+	fill ('firebrick') 
+	triangle(400, 300, 700, 200, 660, 365); //tri
+	
+	fill('mediumpurple');
+	circle(400, 300, 150); // circ
+	fill ('lightsalmon') 
+	//triangle(400, 300, 670, 50, 680, 215); //tri
+	triangle(400, 300, 660, 10, 710, 230);
+	
+	
+	fill('plum');
+	circle(400, 300, 150); // circ
+	
+} 
